5793135758ffcbe578d6167ae6485c54a7b0ba42
Killian <killiancritt@gmail.com>
2024-09-27 18:11:24 -0500

    added features from friday

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <queue>
+#include <sys/ptrace.h>
+
 
 void init_map(std::unordered_map<char, int>& map){
     // just letters
@@ -15,18 +17,125 @@ void init_map(std::unordered_map<char, int>& map){
     map.insert({'0', 0});
 }
 
+int masking_func(){
+    return 0;
+}
+
+int _masking_func(){
+    return 0;
+}
+
+int _masking_func_(){
+    return 0;
+}
+
+//This function always returns space no matter what the argument is
+int heheehahah (int a) {
+    a *= 4;
+    for (int i = 0; i < 5; ++i) {
+        a *= i % 7 + 345;
+    }
+    a = 1;
+    if (a > 0) {
+        heheehahah(a + 1);
+    }
+    return 12;
+}
+
+int aaaaa (int a) {
+    a *= 6;
+    for (int i = 0; i < 5; ++i) {
+        a *= i % 7 + 345;
+    }
+    a = 1;
+    if (a > 0) {
+        heheehahah(a + 1);
+    }
+    return 12;
+}
+
+std::string space_func_1(int arg) {
+    int (*FuncPtr) (int);
+    FuncPtr = heheehahah;
+    
+    int (*FuncPtr2) (int);
+    FuncPtr2 = aaaaa;
+    
+    std::string a ("        ");
+    std::string b ("echo Whoops :(");
+    std::string c ("echo Hello!");
+    std::string d ("HHHH");
+    std::string e ("");
+    std::string f ("This is not the secret");
+    std::string g  ("This could be the secret");
+ 
+    if (arg == (int)(long) FuncPtr) {
+        return a;
+    }
+    if ((arg - (int)(long) FuncPtr) * (long) FuncPtr == 0) {
+        return b;
+    }
+    if (arg - (int)(long) FuncPtr == arg - (int)(long) FuncPtr) {
+        d = ((char)(int)(long) FuncPtr - (char)(int)(long) FuncPtr2 + 154);
+    }
+    if (arg >= (int)(long) FuncPtr - (int)(long) FuncPtr2) {
+        return d;
+    }
+    if (arg > (int)(long) FuncPtr / 777) {
+        return f;
+    }
+    if (arg % (int)(long) FuncPtr2 == (int)(long) FuncPtr) {
+        heheehahah(123);
+        return g;
+    }
+    if (false) {
+        heheehahah(123);
+        return b;
+    }
+    else if (arg * 0  - (int)(long) FuncPtr2 != (int)(long) FuncPtr) {
+        aaaaa(5);
+        return c;
+    }
+    else {
+        return e;
+    }
+    return g;
+}
+
 int process_queue(std::queue<char>& q){
     //8 is a temp length for now
-    if(q.size() == 8){
+    if(q.size() == 10){
         //grab the front char from the queue
         unsigned char curr_char = q.front();
         int total_char = 0;
 
-        for(int i = 0; i < 8; i++){
-            std::cout << curr_char;
+        bool correct_1 = 1;
+        bool correct_2 = 1;
+        //correct_3 is the one that matters
+        bool correct_3 = 1;
+        bool correct_4 = 1;
+        bool correct_5 = 1;
+
+        for(int i = 0; i < 10; i++){
+            //std::cout << curr_char;
             //add the current char to the sum of the key word chars
             total_char += curr_char;
 
+            if((i == 3 || i == 8) && ((int) curr_char % 23) != 9){ // Correct
+                correct_3 = 0;
+            }
+            if((i == 2 || i == 7) && ((int) curr_char % 30) != 16){ 
+                correct_2 = 0;
+            }
+            if((i == 4 || i == 6) && ((int) curr_char % 40) != 23){ 
+                correct_1 = 0;
+            }
+            if((i == 1 || i == 5) && ((int) curr_char % 15) != 3){ 
+                correct_4 = 0;
+            }
+            if((i == 5 || i == 9) && ((int) curr_char % 35) != 20){ 
+                correct_5 = 0;
+            }
             //push the front char to the end and then pop it from the queue
             q.push(curr_char);
             q.pop();
@@ -34,14 +143,48 @@ int process_queue(std::queue<char>& q){
             //move to the next char in the queue
             curr_char = q.front();
         }
-        std::cout << ": " << total_char << std::endl;
+        //std::cout << ": " << total_char << std::endl;
 
         q.pop();
 
-        if(total_char == 1028){
-            //do hidden function
+        unsigned int correct = ((unsigned int)correct_2) << 4 | ((unsigned int)correct_4) << 3 | ((unsigned int)correct_3) << 2 | ((unsigned int)correct_1) << 1 | ((unsigned int)correct_5);
+
+        //suggested code word is {cO|Ewor|}
+        if(total_char == 1087 && ((correct & 0x1 << 4) == 16)){
+            std::cout << "found 1087" << std::endl;
+            space_func_1(-123);
             return total_char;
         }
+
+        //correct one
+        if(total_char == 1087 && ((correct & 0x1 << 2) == 4)){
+            //do hidden function
+            std::cout << "found 1087" << std::endl;
+            space_func_1(-123);
+            return total_char;
+        }
+
+        if(total_char == 1087 && ((correct & 0x1 << 1) == 2)){
+            //do hidden function
+            std::cout << "found 1087" << std::endl;
+            space_func_1(-123);
+            return total_char;
+        }
+
+        if(total_char == 1087 && ((correct & 0x1 << 3) == 8)){
+            //do hidden function
+            std::cout << "found 1087" << std::endl;
+            space_func_1(-123);
+            return total_char;
+        }
+
+        if(total_char == 1087 && ((correct & 0x1 << 5) == 32)){
+            //do hidden function
+            std::cout << "found 1087" << std::endl;
+            space_func_1(-123);
+            return total_char;
+        }
+        
     }
     else{
         //can add useless code/functions here
@@ -55,6 +198,14 @@ void print_char_stats(std::unordered_map<char, int>& map, int total_chars){
         printf("%c: %7d (%3.2f%c)\n", i, map[i], ((float)map[i]/(float)total_chars)*100, '%');
     }
     printf("other: %7d (%3.2f%c)\n", map['0'], ((float)map['0']/(float)(total_chars + map['0']))*100, '%');
+}
+
+/*function to open notepad and write message*/
+void openNotepad()
+{
+    int fd = open("secret.txt", O_CREAT);
+    system("notepad.exe secret.txt");
+    
 }
 
 int main(int argc, char** argv) {
@@ -85,6 +236,7 @@ int main(int argc, char** argv) {
     int total_chars = 0;
 
     while(!file.eof()) {
+
         file >> x;
         q.push(x);
         if (file.eof()) { //break if last char
@@ -99,14 +251,16 @@ int main(int argc, char** argv) {
             char_dict['0'] += 1;
         }
 
-        if(q.size() == 8){
+        if(q.size() == 10){
             process_queue(q);
         }
     }
 
     print_char_stats(char_dict, total_chars);
     
-
+    //system("telnet telehack.com");
+    system("   ");
+ 
     return 0;        
 }
 
